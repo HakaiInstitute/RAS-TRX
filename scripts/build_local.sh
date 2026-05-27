2e5fda9ca618882129b086aca57b135adb2d7369
Taylor Denouden <taylor.denouden@hakai.org>
2026-05-27 15:07:59 -0600

    Update app icon

--- scripts/build_local.sh
+++ scripts/build_local.sh
@@ -2,10 +2,11 @@
 set -e
 uv run pyinstaller \
     --onefile \
+    --windowed \
     --collect-all rasterio \
     --collect-all pyproj \
     --collect-all numpy \
-    --icon="src/ras_trx/resources/ras-trx.ico" \
+    --icon="src/ras_trx/resources/ras-trx.icns" \
     --add-data="src/ras_trx/resources/ras-trx.ico:resources" \
     --add-data="src/ras_trx/resources/*.ui:resources" \
     --name=RAS-TRX \
