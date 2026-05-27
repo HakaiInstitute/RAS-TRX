ea292181cc3b892d42fa7285fa0eded77ac67989
Taylor Denouden <taylor.denouden@hakai.org>
2026-05-27 14:45:31 -0600

    Add script to run local pyinstaller build

---
+++ scripts/build_local.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+uv run pyinstaller \
+    --onefile \
+    --collect-all rasterio \
+    --collect-all pyproj \
+    --collect-all numpy \
+    --icon="src/ras_trx/resources/ras-trx.ico" \
+    --add-data="src/ras_trx/resources/ras-trx.ico:resources" \
+    --add-data="src/ras_trx/resources/*.ui:resources" \
+    --name=RAS-TRX \
+    src/ras_trx/__main__.py
